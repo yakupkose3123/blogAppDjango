02139109578b7dbe1e56033190807e568d55901b
yakupkose3123 <yakupkose3123@gmail.com>
2022-07-11 16:03:38 +0300

    import-export added for admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
+from import_export import resources
 from .models import Category, Post , Comment, Like, PostView
 from import_export.admin import ImportExportModelAdmin
-from blog.resources import PostResource
 
-class PostAdmin(ImportExportModelAdmin):    
+class PostResource(resources.ModelResource):
+        class Meta:
+            model = Post
+class PostAdmin(ImportExportModelAdmin):
     resource_class = PostResource
 
+
+
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(Like)
 admin.site.register(PostView)
--- blog/resources.py
+++
@@ -1,8 +0,0 @@
-from import_export import resources
-from blog.models import Post
-
-class PostResource(resources.ModelResource):
-
-    class Meta:
-        model = Post # default all fields
-        # fields = ("is_released", "product")  
